a5dd81d04d603edf8556234292891b03e2db18a7
SameerD14 <dhanjodeSameer@gmail.com>
2025-09-29 20:44:13 +0530

    Update cloudinary configuration.

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -1,10 +1,23 @@
+//const cloudinary = require("cloudinary").v2;
+//require("dotenv").config({ quiet: true });
+
+//cloudinary.config({
+//	cloud_name: "",
+//	api_key: "",
+//	api_secret: "",
+//});
+
+//module.exports = cloudinary;
+
+
 const cloudinary = require("cloudinary").v2;
 require("dotenv").config({ quiet: true });
 
-cloudinary.config({
-	cloud_name: "",
-	api_key: "",
-	api_secret: "",
-});
+cloudinary.config({   
+	cloud_name: "dv6kpjafa", 
+	api_key: "899745167288953",   
+	api_secret: "W2nhNndF3u9ZZQsqfJI2tFEn24M",
+}); 
 
-module.exports = cloudinary;
+
+module.exports = cloudinary; // CLOUDINARY_URL=cloudinary://899745167288953:W2nhNndF3u9ZZQsqfJI2tFEn24M@dv6kpjafa
