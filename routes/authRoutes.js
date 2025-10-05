f8fe47a2b697698591c1b04905deb32da36174ef
SameerD14 <dhanjodeSameer@gmail.com>
2025-10-05 18:40:20 +0530

    Update authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
+
+//send otp routes 
 router.post("/send-otp", authController.sendOtp);
+
+// verify otp routes
 router.post("/verify-otp", authController.verifyOtpAndLogin);
 
 module.exports = router;
