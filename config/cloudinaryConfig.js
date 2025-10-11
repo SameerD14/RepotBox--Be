6d26ac7435f40d0dd0beb5083dd2a947dab8fee0
Sameer Dhanjode <dhanjodesameer@gmail.com>
2025-10-11 16:17:10 +0530

    mail address and password update

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -1,15 +1,3 @@
-//const cloudinary = require("cloudinary").v2;
-//require("dotenv").config({ quiet: true });
-
-//cloudinary.config({
-//	cloud_name: "",
-//	api_key: "",
-//	api_secret: "",
-//});
-
-//module.exports = cloudinary;
-
-
 const cloudinary = require("cloudinary").v2;
 require("dotenv").config({ quiet: true });
 
--- utils/sendOtpMail.js
+++ utils/sendOtpMail.js
@@ -4,16 +4,14 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
 	service: "gmail",
 	auth: {
-		// user: "himanshu13khade@gmail.com",
-		// pass: "vzml sixt fsdu ogno",
-		user: "y8221733@gmail.com",
+       user: "y8221733@gmail.com",
 		pass: "ajto nkzy opgd ivfg",
 	},
 });
 
 exports.sendOtpMail = async (email, otp) => {
 	const mailOptions = {
-		from: "himanshu13khade@gmail.com",
+		from: "y8221733@gmail.com",
 		to: email,
 		subject: "Your OTP for Login",
 		text: `Your OTP is ${otp}. It will expire in 10 minutes.`,
